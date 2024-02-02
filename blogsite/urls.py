6f17a82dd6640392325dff966689b46b0cf78319
Mathis_Rimbert <mathis.rimbert@centrale.centralelille.fr>
2024-02-02 14:03:00 +0100

    Remove useless url

--- blogsite/urls.py
+++ blogsite/urls.py
@@ -5,7 +5,6 @@ from . import views
 app_name = "blogsite"
 urlpatterns = [
         path("", views.index, name="index"),
-        path("/", views.index, name="index-button"),
         path("article/", views.article, name="article"),
         path("<int:article_id>/detail", views.detail, name="detail"),
         path("contact/", views.contact, name="contact"),
