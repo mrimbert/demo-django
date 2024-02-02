53855dc02205a91dfa782fc2ffc805220e7cc1f5
Mathis_Rimbert <mathis.rimbert@centrale.centralelille.fr>
2024-02-02 11:29:11 +0100

    Form update

--- blogsite/forms.py
+++ blogsite/forms.py
@@ -20,27 +20,28 @@ class ContactForm(ModelForm):
         fields = ['nom', 'prenom', 'tel', 'sujet', 'message']
         widgets = {
             'nom' : TextInput(attrs = {
-                'class':"block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer",
+                'class':"block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer",
                 'style':"max-width:500px;",
                 'placeholder':"Entrez votre nom",
             }),
             'prenom' : TextInput(attrs = {
-                'class':"block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer",
+                'class':"block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer",
                 'style':"max-width:500px;",
                 'placeholder':"Entrez votre prénom",
             }),
             'tel' : TextInput(attrs = {
-                'class': "block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer",
+                'class': "block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer",
                 'style':"max-width:300px;",
+                'pattern':"[0-9]{2} [0-9]{2} [0-9]{2} [0-9]{2} [0-9]{2}",
                 'placeholder':"Entrez votre téléphone",
             }),
             'sujet' : TextInput(attrs = {
-                'class':"block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer",
+                'class':"block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer",
                 'style':"max-width:300px;",
                 'placeholder':"Saisir le sujet du message",
             }),
             'message' : Textarea(attrs = {
-                'class':"block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer",
+                'class':"block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-non focus:outline-none focus:ring-0 focus:border-blue-600 peer",
                 'style':"max-width:500px;",
                 'placeholder':"Saisissez-votre message",
             })
