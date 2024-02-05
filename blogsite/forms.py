f6183f24fda7a478c5927894007c9da75bf0d20f
Mathis <mathis.rimbert@centrale.centralelille.fr>
2024-02-05 16:10:40 +0100

    add style on forms

--- blogsite/forms.py
+++ blogsite/forms.py
@@ -9,7 +9,6 @@ class MailForm(ModelForm):
         widgets = {
             'mail' : EmailInput(attrs={
                 'class': "text-black w-96 rounded-lg",
-                'style': "max-width:300px;",
                 'placeholder':"Entrez votre e-mail"
             })
         }
@@ -20,30 +19,25 @@ class ContactForm(ModelForm):
         fields = ['nom', 'prenom', 'tel', 'sujet', 'message']
         widgets = {
             'nom' : TextInput(attrs = {
-                'class':"block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer",
                 'style':"max-width:500px;",
-                'placeholder':"Entrez votre nom",
+                'placeholder':"Nom",
             }),
             'prenom' : TextInput(attrs = {
-                'class':"block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer",
                 'style':"max-width:500px;",
-                'placeholder':"Entrez votre prénom",
+                'placeholder':"Prénom",
             }),
             'tel' : TextInput(attrs = {
-                'class': "block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer",
                 'style':"max-width:300px;",
                 'pattern':"[0-9]{2} [0-9]{2} [0-9]{2} [0-9]{2} [0-9]{2}",
-                'placeholder':"Entrez votre téléphone",
+                'placeholder':"Tél (ex : 01-02-03-04-05)",
             }),
             'sujet' : TextInput(attrs = {
-                'class':"block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer",
                 'style':"max-width:300px;",
-                'placeholder':"Saisir le sujet du message",
+                'placeholder':"Sujet du message",
             }),
             'message' : Textarea(attrs = {
-                'class':"block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer",
                 'style':"max-width:500px;",
-                'placeholder':"Saisissez-votre message",
+                'placeholder':"Message",
             })
 
 
--- testdjango/settings.py
+++ testdjango/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'widget_tweaks',
     'theme',
     'tailwind',
     'blogsite.apps.BlogsiteConfig',
