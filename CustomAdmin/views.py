0582874a5b4ea80801fc8105b4fbe3a7b3a51f5f
Mathis <mathis.rimbert@centrale.centralelille.fr>
2024-02-07 10:03:37 +0100

    add chart like/article

--- CustomAdmin/views.py
+++ CustomAdmin/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
 
+import json
+
 from django.template.response import TemplateResponse
+
+from blogsite.models import Article
+
  
  
 def custom_app_view(request):
+    article = list(Article.objects.values_list('title'))
+    like = list(Article.objects.values_list('popular'))
     context = {'title': 'Statistique d\'utilisation', 'site_title': 'Custom App View', 'site_header': 'Informations générales',
-               'index_title': 'Informations administrateurs', 'text': 'Custom App Text'}
+               'index_title': 'Informations administrateurs', 'text': 'Custom App Text', 'article':json.dumps(article), 'like':json.dumps(like)}
     return TemplateResponse(request, 'admin/custom_app_view.html', context)
